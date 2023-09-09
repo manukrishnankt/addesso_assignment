60bce381d2169727a4fb4fbca45cb3a5257cea82
manukrishnankt <manukrishnankt@gmail.com>
2023-09-09 22:45:43 +0530

    un necessary import

--- src/main/java/com/addesso/assignment/repository/InsuranceContractRepository.java
+++ src/main/java/com/addesso/assignment/repository/InsuranceContractRepository.java
@@ -1,7 +1,6 @@
 package com.addesso.assignment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.addesso.assignment.model.InsuranceContract;
 
